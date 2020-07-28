d9de52117525a3beefe66bf9645f88937771512a
Ahmed Tarek <a7md.tare2@gmail.com>
2020-07-28 05:16:23 +0200

    Updates

---
+++ precache-manifest.913028c32f708673f8ec04436aab47fd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72338b2f5cb7d7aebb1302be1c62c9a7",
+    "url": "/webdev/index.html"
+  },
+  {
+    "revision": "be4fc97897f773adab3a",
+    "url": "/webdev/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c6b4f4ccf8c2f32f1745",
+    "url": "/webdev/static/css/main.b2ba579f.chunk.css"
+  },
+  {
+    "revision": "be4fc97897f773adab3a",
+    "url": "/webdev/static/js/2.1a83dd38.chunk.js"
+  },
+  {
+    "revision": "24b94c9b28f4c11b82c117a6b23cd9d4",
+    "url": "/webdev/static/js/2.1a83dd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b4f4ccf8c2f32f1745",
+    "url": "/webdev/static/js/main.f3fb7ad5.chunk.js"
+  },
+  {
+    "revision": "cb15d6cbbc1a2224e926",
+    "url": "/webdev/static/js/runtime-main.e00cce63.js"
+  },
+  {
+    "revision": "ecb6225812a0475dfd7ee2b55154c858",
+    "url": "/webdev/static/media/0.ecb62258.PNG"
+  },
+  {
+    "revision": "00dd54ed1b65ab0f7716a7924eebbb47",
+    "url": "/webdev/static/media/1.00dd54ed.PNG"
+  },
+  {
+    "revision": "51a86086075545b868b342b505d43dfb",
+    "url": "/webdev/static/media/2.51a86086.PNG"
+  },
+  {
+    "revision": "02a22d134e89fff594997e0c565c83e0",
+    "url": "/webdev/static/media/3.02a22d13.PNG"
+  },
+  {
+    "revision": "269955ebcb68e3fc455a31c2c4c91399",
+    "url": "/webdev/static/media/background.269955eb.jpg"
+  },
+  {
+    "revision": "71dd009941483e190e7c1d90867268ff",
+    "url": "/webdev/static/media/map-image.71dd0099.jpg"
+  },
+  {
+    "revision": "3d0484463940d95cf12ef80ab698fd5f",
+    "url": "/webdev/static/media/me.3d048446.jpg"
+  },
+  {
+    "revision": "3a2e86c995236605228dd7793fee8a88",
+    "url": "/webdev/static/media/whatsapp.3a2e86c9.svg"
+  }
+]);
